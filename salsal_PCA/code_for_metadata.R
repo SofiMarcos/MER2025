e2c9a4f5c0bee33ad3f5321e6a64dfc57b4870b5
sof <sofia.marcos.ba@gmail.com>
2024-02-29 17:33:36 +0100

    corrected excel

--- salsal_PCA/code_for_metadata.R
+++ salsal_PCA/code_for_metadata.R
@@ -1,8 +1,11 @@
 # Script to add metadata to the PCA table
+# Load metadata
 pop <- read.table("Salmo_salar_localities.csv", sep = ";", header = TRUE)
 
+# Check if sample order in the metadata file is the same as in the pca dataframe
 pop$Sample == rownames(df2)
 
+# Add two columns to the pca dataframe
 df2$subpopulation <- pop$Subpopulation
 df2$population <- pop$Population
 
